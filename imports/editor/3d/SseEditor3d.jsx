23747eb89833924313147f0d4b01c8e299b260a3
LoadingDeao <chuzhiwei@suanier.com>
2019-05-09 17:55:28 +0800

    test orthCamera

--- imports/editor/3d/SseEditor3d.jsx
+++ imports/editor/3d/SseEditor3d.jsx
@@ -561,8 +561,7 @@ export default class SseEditor3d extends React.Component {
 
         // const camera = this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 10000);
         //orthCamera
-        const Camera = this.camera = new THREE.OrthographicCamera(window.innerWidth/-14.5,window.innerWidth/14.5,
-            window.innerHeight/14.5,window.innerHeight/-14.5,-100 , 10000);
+        const Camera = this.camera = new THREE.OrthographicCamera(window.innerWidth/-2,window.innerWidth/2,window.innerHeight/2,window.innerHeight/-2,1,1000);
 
         scene.add(camera);
 
