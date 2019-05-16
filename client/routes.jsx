8ba72faf92a5bf9004fef05c7a962577dbe60200
LoadingDeao <chuzhiwei@suanier.com>
2019-05-16 13:42:25 +0800

    accounts test

--- client/routes.jsx
+++ client/routes.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import {Route, Router, Redirect} from 'react-router';
 import createBrowserHistory from 'history/createBrowserHistory';
 import SseEditorApp from "../imports/editor/SseEditorApp";
@@ -6,13 +7,18 @@ import SseNavigatorApp from "../imports/navigator/SseNavigatorApp";
 import SseAllAnnotated from "../imports/navigator/SseAllAnnotated";
 
 const browserHistory = createBrowserHistory();
+var basepath = "/browse/0/20/"
+if(Meteor.userId())
+{
+    basepath += Meteor.userId()
+}
 export const renderRoutes = () => (
     <Router history={browserHistory}>
         <div>
-            <Route exact path="/" render={()=>(<Redirect to="/browse/0/20/"/>)}/>
+            <Route exact path="/" render={()=>(<Redirect to={basepath} />)}/>
             <Route path="/edit/:path" component={SseEditorApp}/>
-            <Route exact path="/edit/" render={()=>(<Redirect to="/browse/0/20/"/>)}/>
-            <Route exact path="/edit" render={()=>(<Redirect to="/browse/0/20/"/>)}/>
+            <Route exact path="/edit/" render={()=>(<Redirect to={basepath}/>)}/>
+            <Route exact path="/edit" render={()=>(<Redirect to={basepath}/>)}/>
             <Route path="/browse/:fromIndex/:pageLength/:path?" component={SseNavigatorApp}/>
             <Route path="/annotated" component={SseAllAnnotated}/>
         </div>
