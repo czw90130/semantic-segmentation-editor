1ba49bfca8399774098c74528154fc083cabe474
LoadingDeao <chuzhiwei@suanier.com>
2019-05-17 17:54:51 +0800

    accounts test

--- imports/common/AccountsUIWrapper.js
+++ imports/common/AccountsUIWrapper.js
@@ -4,15 +4,23 @@ import { Template } from 'meteor/templating';
 import { Blaze } from 'meteor/blaze';
  
 export default class AccountsUIWrapper extends Component {
-  componentDidMount() {
+  componentDidMount()
+  {
     // Use Meteor Blaze to render login buttons
     this.view = Blaze.render(Template.loginButtons,
       ReactDOM.findDOMNode(this.refs.container));
   }
-  componentWillUnmount() {
+  componentWillUnmount()
+  {
     // Clean up Blaze view
     Blaze.remove(this.view);
     console.log("componentWillUnmount")
+  }
+  componentWillReceiveProps(nextProps)
+  {
+    console.log("componentWillReceiveProps")
+    console.log(this.props)
+    console.log(nextProps)
   }
   render() {
     // Just render a placeholder container that will be filled in
