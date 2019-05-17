be1f4343e6666fa3db268ffda6b6ed693b9bb074
LoadingDeao <chuzhiwei@suanier.com>
2019-05-17 16:22:50 +0800

    accounts test

--- client/routes.jsx
+++ client/routes.jsx
@@ -49,7 +49,7 @@ export const renderRoutes = function()
             <Route exact path="/edit" render={()=>(<Redirect to={basepath}/>)}/>
             <Route path="/browse/:fromIndex/:pageLength/:path?" component={SseNavigatorApp}/>
             <Route path="/annotated" component={SseAllAnnotated}/>
-            <Route exact path="/notautorized" render={()=>(<Redirect to="/browse/0/20/%2Fnotautorized" />)}/>
+            <Route path="/notautorized" render={()=>(<Redirect to="/browse/0/20/%2Fnotautorized" />)}/>
         </div>
     </Router>
 );
